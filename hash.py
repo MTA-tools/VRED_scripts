bc17302039cf3f4142955622362dde9418b82363
met <mta@kali>
2025-03-21 01:13:38 +0000

    Prettified output of hash.py

--- hash.py
+++ hash.py
@@ -12,21 +12,50 @@ def ror_str(byte, count):
     return int(binb, 2)
 
 
-if __name__ == "__main__":
-    try:
-        esi = sys.argv[1]
-    except IndexError:
-        print(f"[-] Usage: {sys.argv} input_string!")
-        sys.exit()
-
-    # Initialize variables
+def long_hash(esi, key):
     edx = 0x00
     ror_count = 0
-
     for eax in esi:
         edx = edx + ord(eax)
         if ror_count < len(esi) - 1:
-            edx = ror_str(edx, 0xD)
+            edx = ror_str(edx, key)
         ror_count += 1
+    
+    return edx
 
-    print(f"[+] Hash = {hex(edx)}")
+
+def short_hash(esi, key):
+    dl = 0
+    for char in esi:
+        al = ord(char)
+        al ^= key
+        al &= 0xff
+        dl -= al
+        dl &= 0xff
+    
+    # null byte
+    dl -= key
+    dl &= 0xff
+    return dl
+    
+
+def main():
+    esi = sys.argv[1]
+
+    long_key = 0xd
+    kernel32_key    = 0xf8
+    ws2_32_key      = 0xc0
+    short_key       = 0x71
+
+    print("+-------+------------+")
+    print("| Key   | Hash Value |")
+    print("+-------+------------+")
+    print(f"| {hex(long_key)}   | {hex(long_hash(esi, long_key)):>10} |")
+    print(f"| {hex(kernel32_key)}  | {hex(short_hash(esi, kernel32_key)):>10} |")
+    print(f"| {hex(ws2_32_key)}  | {hex(short_hash(esi, ws2_32_key)):>10} |")
+    print(f"| {hex(short_key)}  | {hex(short_hash(esi, short_key)):>10} |")
+    print("+-------+------------+")
+
+if __name__ == "__main__":
+    main()
+
